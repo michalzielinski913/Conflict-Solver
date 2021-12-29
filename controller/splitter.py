128cf9d1dacb4c6844057a2f59fc77a658d6b770
Aleksander Świtała <olek5465@gmail.com>
2021-12-29 17:15:38 +0100

    Fix

--- controller/splitter.py
+++ controller/splitter.py
@@ -1,4 +1,5 @@
 import re
+import time
 
 class Splitter:
     def chopAfterNChars(data, n):
@@ -13,11 +14,24 @@ class Splitter:
         return data
 
     def chopAfterNSentence(data, n):
-        regex = ".*[!?.]{1," + str(n) + "}"
-        data = re.findall(regex, data)
+        regex = "(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?|\!)\s"
+        data = re.split(regex, data)
+        data = [' '.join(data[i: i + n]) for i in range(0, len(data), n)]
         return data
 
     def chopAfterNLines(data, n):
         data = data.splitlines(True)
         data = [' '.join(data[i:i+n]) for i in range(0,len(data),n)]
         return data
+
+# file = open("enwik8", "r")
+file = open("długieLoremIpsum2.txt", "r")
+data = file.read()
+temp = Splitter
+start = time.time()
+# print(temp.chopAfterNChars(data, 10))
+# print(temp.chopAfterNWords(data, 10))
+print(temp.chopAfterNSentence(data, 3))
+# print(temp.chopAfterNLines(data, 2))
+end = time.time()
+print(end - start)
