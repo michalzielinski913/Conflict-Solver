3bdd4a613456516ee6a495b8271b2ad4b56ffea7
Michał Zieliński <michalmaniak2000@gmail.com>
2021-12-30 12:44:22 +0100

    Merge
    Integrated FrontEndDevelopment branch with splitter from master branch

--- controller/Controller.py
+++ controller/Controller.py
@@ -1,22 +1,25 @@
+from controller.splitter import Splitter
 from model.txtFile import txtFile
 import tkinter as tk
 from tkinter import filedialog
 import eel
+
+
 class Controler:
 
     def __init__(self):
-        pass
+        self.splitter = Splitter()
 
     def setSplitSettings(self, mode, N):
-        self._mode=mode
-        self._N=N
+        self._mode = mode
+        self._N = N
 
     def getSplitSettings(self):
         return self._mode, self._N
 
     def loadFiles(self, pathOne, pathTwo):
-        self.fileOne=txtFile(pathOne)
-        self.fileTwo=txtFile(pathTwo)
+        self.fileOne = txtFile(pathOne)
+        self.fileTwo = txtFile(pathTwo)
 
     def chooseFilePath(self, wildcard="*"):
         root = tk.Tk()
@@ -34,50 +37,63 @@ class Controler:
         eel.setLeftText(self.dictT[int(id)][0])
         eel.setRightText(self.dictT[int(id)][1])
 
-
     def cancelCompare(self):
-        self.cancel=True
+        self.cancel = True
         pass
 
-
     def loadFiles(self, pathOne, pathTwo):
-        print(pathOne)
-        print(pathTwo)
         self.dictT = {}
-        self.cancel=False
+        self.cancel = False
         x = 1
         jsonS = ""
         jsonElements = []
-        with open(pathOne) as file1, open(pathTwo) as file2:
-            for line1, line2 in zip(file1, file2):
-                if self.cancel==True:
-                    return
-                if (x % 1000 == 0):
-                    jsonS = jsonS[:-1]
-                    jsonS = jsonS.replace("\"", "\\\"")
-                    jsonS = jsonS.replace("\'", "\"")
-                    jsonS = "[" + jsonS + "]"
-                    eel.addRows(jsonS)
-                    eel.sleep(0.5)
-                    jsonS = ""
-                line1 = line1.replace("\'", "\"")
-                line2 = line2.replace("\'", "\"")
-                jsonElement = {
-                    "DT_RowId": x,
-                    "FirstFile": line1[:33],
-                    "SecondFile": line2[:33]
-                }
-                jsonS = jsonS + str(jsonElement) + ","
-                row = [line1, line2]
-                self.dictT[x] = row
-                x += 1
+        fileOne = open(pathOne, "r")
+        fileTwo = open(pathTwo, "r")
+        dataOne = fileOne.read()
+        dataTwo = fileTwo.read()
 
-            for element in jsonElements:
-                jsonS = jsonS + str(element) + ","
+        if self._mode == "line":
+            partOne = self.splitter.chopAfterNLines(dataOne, self._N)
+            partTwo = self.splitter.chopAfterNLines(dataTwo, self._N)
+        elif self._mode == "char":
+            partOne = self.splitter.chopAfterNChars(dataOne, self._N)
+            partTwo = self.splitter.chopAfterNChars(dataTwo, self._N)
+        elif self._mode == "word":
+            partOne = self.splitter.chopAfterNWords(dataOne, self._N)
+            partTwo = self.splitter.chopAfterNWords(dataTwo, self._N)
+        elif self._mode == "sentence":
+            partOne = self.splitter.chopAfterNSentence(dataOne, self._N)
+            partTwo = self.splitter.chopAfterNSentence(dataTwo, self._N)
 
-            jsonS = jsonS[:-1]
-            jsonS = jsonS.replace("\"", "\\\"")
-            jsonS = jsonS.replace("\'", "\"")
-            jsonS = "[" + jsonS + "]"
-            jsonElements = []
-            eel.addRows(jsonS)
+        for elementOne, elementTwo in zip(partOne, partTwo):
+            if self.cancel == True:
+                return
+            if (x % 1000 == 0):
+                jsonS = jsonS[:-1]
+                jsonS = jsonS.replace("\"", "\\\"")
+                jsonS = jsonS.replace("\'", "\"")
+                jsonS = "[" + jsonS + "]"
+                eel.addRows(jsonS)
+                eel.sleep(0.5)
+                jsonS = ""
+            line1 = elementOne.replace("\'", "\"")
+            line2 = elementTwo.replace("\'", "\"")
+            jsonElement = {
+                "DT_RowId": x,
+                "FirstFile": line1[:33],
+                "SecondFile": line2[:33]
+            }
+            jsonS = jsonS + str(jsonElement) + ","
+            row = [line1, line2]
+            self.dictT[x] = row
+            x += 1
+
+        for element in jsonElements:
+            jsonS = jsonS + str(element) + ","
+
+        jsonS = jsonS[:-1]
+        jsonS = jsonS.replace("\"", "\\\"")
+        jsonS = jsonS.replace("\'", "\"")
+        jsonS = "[" + jsonS + "]"
+        jsonElements = []
+        eel.addRows(jsonS)
--- controller/splitter.py
+++ controller/splitter.py
@@ -2,36 +2,24 @@ import re
 import time
 
 class Splitter:
-    def chopAfterNChars(data, n):
+    def chopAfterNChars(self, data, n):
         regex = ".{1," + str(n) + "}"
         print(regex)
         data = re.findall(regex, data)
         return data
 
-    def chopAfterNWords(data, n):
+    def chopAfterNWords(self, data, n):
         data = data.split()
         data = [' '.join(data[i: i + n]) for i in range(0, len(data), n)]
         return data
 
-    def chopAfterNSentence(data, n):
+    def chopAfterNSentence(self, data, n):
         regex = "(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?|\!)\s"
         data = re.split(regex, data)
         data = [' '.join(data[i: i + n]) for i in range(0, len(data), n)]
         return data
 
-    def chopAfterNLines(data, n):
+    def chopAfterNLines(self, data, n):
         data = data.splitlines(True)
         data = [' '.join(data[i:i+n]) for i in range(0,len(data),n)]
         return data
-
-# file = open("enwik8", "r")
-file = open("długieLoremIpsum2.txt", "r")
-data = file.read()
-temp = Splitter
-start = time.time()
-# print(temp.chopAfterNChars(data, 10))
-# print(temp.chopAfterNWords(data, 10))
-print(temp.chopAfterNSentence(data, 3))
-# print(temp.chopAfterNLines(data, 2))
-end = time.time()
-print(end - start)
