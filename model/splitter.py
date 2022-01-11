830183d43dfbdc0090a49ee51215479e22a1d56a
Aleksander Świtała <olek5465@gmail.com>
2022-01-11 22:46:30 +0100

    Corrected syntax

--- model/splitter.py
+++ model/splitter.py
@@ -3,24 +3,24 @@ import time
 import os
 
 class Splitter:
-    def chopAfterNChars(data, n):
+    def chop_after_n_chars(data, n):
         regex = ".{1," + str(n) + "}"
         data = data.replace('\n', '')
         data = re.findall(regex, data)
         return data
 
-    def chopAfterNWords(data, n):
+    def chop_after_n_words(data, n):
         data = data.split()
         data = [' '.join(data[i: i + n]) for i in range(0, len(data), n)]
         return data
 
-    def chopAfterNSentence(data, n):
+    def chop_after_n_sentence(data, n):
         regex = "(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?|\!)\s"
         data = re.split(regex, data)
         data = [' '.join(data[i: i + n]) for i in range(0, len(data), n)]
         return data
 
-    def chopAfterNLines(data, n):
+    def chop_after_n_lines(data, n):
         data = data.splitlines(True)
         data = [''.join(data[i:i+n]) for i in range(0,len(data),n)]
         return data
