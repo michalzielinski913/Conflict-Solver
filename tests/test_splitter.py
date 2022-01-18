b18a0dad30ceaa3fcf9f9f64397974289ea2a5c2
MikolajDzi <mikolaj.dziubinski@interia.pl>
2022-01-18 18:37:22 +0100

    Added parameterized version of unit test for chop methods

--- tests/test_splitter.py
+++ tests/test_splitter.py
@@ -1,8 +1,12 @@
 import unittest
 import os
+import time
 from model.splitter import Splitter
 
+
 data_path = (os.path.join(os.path.dirname(__file__), "TestFiles"))
+input_filename_list = []
+output_filename_list = []
 
 
 def load_files_input(num):
@@ -25,6 +29,7 @@ def load_files_input(num):
                 with open(os.path.join(root, file), 'r') as f:
                     text = f.read()
                     input_list.append(text)
+                    input_filename_list.append(file)
     return input_list
 
 
@@ -48,57 +53,118 @@ def load_files_output(num):
                 with open(os.path.join(root, file), 'r') as f:
                     text = f.read().encode().decode('unicode_escape')
                     output_list.append(text)
+                    output_filename_list.append(file)
     return output_list
 
 
 class TestSplitter(unittest.TestCase):
 
     def test_chop_after_n_chars(self):
+        start = time.perf_counter()
         splitter = Splitter
         num = 1
         input_list = load_files_input(num)
         output_list = load_files_output(num)
-        for i in range(0, 3):
-            if i < 2:
+        for i in range(len(input_list)):
+            if "Fifty" in input_filename_list[i]:
                 test_result = splitter.chop_after_n_chars(input_list[i], 12)
-            else:
+            elif "Ten" in input_filename_list[i]:
                 test_result = splitter.chop_after_n_chars(input_list[i], 3)
+            elif "Long" in input_filename_list[i]:
+                test_result = splitter.chop_after_n_chars(input_list[i], 1)
+            else:
+                pass
             output = ("\n".join(test_result))
-            self.assertEqual(output_list[i], output)
+            self.assertEqual(output_list[i], output, "Test failed with files " + input_filename_list[i] + " and "
+                             + output_filename_list[i] + ".")
+        stop = time.perf_counter()
+        print(f" test_chop_after_n_chars elapsed is {start - stop:0.4f} seconds. Tested with "
+              + "{}".format(len(input_list)) + " files.")
+        print(input_filename_list)
+        print(output_filename_list)
+        input_filename_list.clear()
+        output_filename_list.clear()
 
     def test_chop_after_n_words(self):
+        start = time.perf_counter()
         splitter = Splitter
         num = 2
         input_list = load_files_input(num)
         output_list = load_files_output(num)
-        for i in range(0, 3):
-            if i < 2:
+        for i in range(len(input_list)):
+            if "Fifty" in input_filename_list[i]:
                 test_result = splitter.chop_after_n_words(input_list[i], 12)
-            else:
+            elif "Ten" in input_filename_list[i]:
                 test_result = splitter.chop_after_n_words(input_list[i], 3)
+            elif "Long" in input_filename_list[i]:
+                test_result = splitter.chop_after_n_words(input_list[i], 1)
+            else:
+                pass
             output = ("\n".join(test_result))
-            self.assertEqual(output_list[i], output)
+            self.assertEqual(output_list[i], output, "Test failed with files " + input_filename_list[i] + " and "
+                             + output_filename_list[i] + ".")
+
+        stop = time.perf_counter()
+        print(f" test_chop_after_n_words elapsed is {start - stop:0.4f} seconds. Tested with "
+              + "{}".format(len(input_list)) + " files.")
+        print(input_filename_list)
+        print(output_filename_list)
+        input_filename_list.clear()
+        output_filename_list.clear()
 
     def test_chop_after_n_sentence(self):
+        start = time.perf_counter()
         splitter = Splitter
         num = 3
         input_list = load_files_input(num)
         output_list = load_files_output(num)
-        for i in range(0, 3):
-            if i < 2:
+        for i in range(len(input_list)):
+            if "Fifty" in input_filename_list[i]:
                 test_result = splitter.chop_after_n_sentence(input_list[i], 12)
-            else:
+            elif "Ten" in input_filename_list[i]:
                 test_result = splitter.chop_after_n_sentence(input_list[i], 3)
+            elif "Long" in input_filename_list[i]:
+                test_result = splitter.chop_after_n_sentence(input_list[i], 1)
+            else:
+                pass
             output = ("\n".join(test_result))
-            self.assertEqual(output_list[i], output)
+            self.assertEqual(output_list[i], output, "Test failed with files " + input_filename_list[i] + " and "
+                             + output_filename_list[i] + ".")
+
+        stop = time.perf_counter()
+        print(f" test_chop_after_n_sentence elapsed is {start - stop:0.4f} seconds. Tested with "
+              + "{}".format(len(input_list)) + " files.")
+        print(input_filename_list)
+        print(output_filename_list)
+        input_filename_list.clear()
+        output_filename_list.clear()
 
     def test_chop_after_n_lines(self):
+        start = time.perf_counter()
         splitter = Splitter
         temp_arr = [5, 12, 1, 3]
         num = 4
         input_list = load_files_input(num)
         output_list = load_files_output(num)
-        for i in range(0, 3):
-            test_result = splitter.chop_after_n_lines(input_list[i], temp_arr[i])
+        for i in range(len(input_list)):
+            if "One" or "Long" in input_filename_list[i]:
+                test_result = splitter.chop_after_n_lines(input_list[i], 1)
+            elif "Three" in input_filename_list[i]:
+                test_result = splitter.chop_after_n_lines(input_list[i], 3)
+            elif "Five" in input_filename_list[i]:
+                test_result = splitter.chop_after_n_lines(input_list[i], 5)
+            elif "Twelve" in input_filename_list[i]:
+                test_result = splitter.chop_after_n_lines(input_list[i], 12)
+            else:
+                pass
             output = ("".join(test_result))
-            self.assertEqual(output_list[i], output)
+            self.assertEqual(output_list[i], output, "Test failed with files " + input_filename_list[i] + " and "
+                             + output_filename_list[i] + ".")
+
+        stop = time.perf_counter()
+        print(f" test_chop_after_n_lines elapsed is {start - stop:0.4f} seconds. Tested with "
+              + "{}".format(len(input_list)) + " files.")
+        print(input_filename_list)
+        print(output_filename_list)
+        input_filename_list.clear()
+        output_filename_list.clear()
