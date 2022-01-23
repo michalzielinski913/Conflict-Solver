0be42425d1ee395cfaf1805b0f7e15ae5d61b7d7
Michał Zieliński <michalmaniak2000@gmail.com>
2022-01-23 16:19:26 +0100

    Remodelled test file
    Test now checks if output json is correct

--- tests/test_compare.py
+++ tests/test_compare.py
@@ -4,25 +4,33 @@ from parameterized import parameterized_class
 import os
 import unittest
 
+from model.Comparator import Comparator
+
 data_path = (os.path.join(os.path.dirname(__file__), "compareTestFiles"))
 
 class TestCompareUnitTest(unittest.TestCase):
 
-    @parameterized_class(('first_file', 'second_file', 'expected'), [
-        ("File1.txt", "File1.txt", [0,8,16])
-        ("File2.txt", "File2.txt", [3,6,43]),
-        ("File3.txt", "File3.txt", [3,22,57,60]),
-        ("File4.txt", "File4.txt", [0,13,34,43]),
-        ("File5.txt", "File5.txt", [1,6,14,25]),
-        ("File6.txt", "File6.txt", [14,15,26,57]),
-        ("File7.txt", "File7.txt", [0,3,4]),
-        ("File8.txt", "File8.txt", [4,18,19])
+    @parameterized_class(('first_file', 'second_file', 'expected_json'), [
+        ("File1.txt", "File1.txt", "json1.txt"),
+        ("File2.txt", "File2.txt", "json2.txt"),
+        ("File3.txt", "File3.txt", "json3.txt"),
+        ("File4.txt", "File4.txt", "json4.txt"),
+        ("File5.txt", "File5.txt", "json5.txt"),
+        ("File6.txt", "File6.txt", "json6.txt"),
+        ("File7.txt", "File7.txt", "json7.txt"),
+        ("File8.txt", "File8.txt", "json8.txt")
     ])
     class TestCompareClass(unittest.TestCase):
         def test_compare(self):
+            comp=Comparator()
+
             with open(data_path + "/firstFile/" + self.first_file, 'r') as first_file:
                 first_text = first_file.readlines()
             with open(data_path + "/secondFile/" + self.second_file, 'r') as second_file:
                 second_text = second_file.readlines()
-            difference_list = compare(first_text, second_text)  # CHANGE METHOD NAME TO CORRECT ONE / METHOD SHOULD RETURN LIST WITH INDEXES WHERE DIFFERENCE OCCURES
-            self.assertEqual(difference_list, self.expected)
+            with open(data_path+ "/json/" +self.expected_json, 'r') as json_file:
+                expected= json_file.read()
+            comp.compare(first_text, second_text)
+            json=comp.getJson()
+            comp.resetJson()
+            self.assertEqual(json, expected)
